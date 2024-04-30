46afb656f2dcf6361a59bb5c2c470e09d1b06005
Elina <elina.vankempen@gmail.com>
2024-04-29 23:31:04 -0700

    making running the secure aggregation a subprocess

---
+++ coord.py
@@ -0,0 +1,69 @@
+import subprocess
+import numpy
+
+from dataclasses import dataclass, field
+from multiprocessing import Process, Queue, Value
+from socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR, IPPROTO_TCP, TCP_NODELAY
+from typing import Any
+from enum import Enum
+
+import logging
+import secrets
+import sys
+import time
+
+from roast.shamir import split_secret
+from roast.model import ActionType, CoordinatorModel
+from roast.roast import share_val, verify
+from roast.transport import send_obj, recv_obj
+
+from roast import fastec
+from roast.coordinator import *
+
+#setup
+
+
+# run the secure aggregation
+print("Start Sec Agg")
+subprocess.call(['sh', './run.sh'])
+print("End Sec Agg")
+
+
+
+#make t signature
+
+logging.basicConfig(level=logging.INFO)
+
+if len(sys.argv) != 7:
+    print(f'usage: {sys.argv[0]} <host> <start_port> <threshold> <total> <malicious> <attacker_level>')
+    sys.exit(1)
+
+host = sys.argv[1]
+start_port = int(sys.argv[2])
+t = int(sys.argv[3])
+n = int(sys.argv[4])
+m = int(sys.argv[5])
+attacker_level = AttackerLevel(int(sys.argv[6]))
+
+msg = b""
+i_to_addr = {i + 1: (host, start_port + i) for i in range(n)}
+ # This is insecure; in practice we'd use DKG, but since
+    # key generation is not the focus of the ROAST protocol, we will
+    # keep the implementation simple by having the coordinator
+    # act as a centralized dealer.
+sk = 1 + secrets.randbelow(fastec.n - 1)
+i_to_sk = split_secret(sk, t, n)
+
+X = sk * fastec.G
+i_to_X = {i: sk_i * fastec.G for i, sk_i in i_to_sk.items()}
+i_to_cached_ctx = {i + 1: Queue() for i in range(n)}
+
+actions = Queue()
+outgoing = Queue()
+coordinator = Coordinator(actions, outgoing, i_to_cached_ctx)
+coordinator.setup(i_to_addr)
+
+model = CoordinatorModel(X, i_to_X, t, n, msg)
+attacker_strategy = AttackerStrategy(attacker_level, n, m)
+elapsed, send_count, recv_count, sid = coordinator.run(i_to_sk, model, attacker_strategy)
+print(t, n, m, attacker_level, elapsed, send_count, recv_count, sid, sep=',')
---
+++ part.py
@@ -0,0 +1,42 @@
+from multiprocessing import Process, Queue
+from socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR, IPPROTO_TCP, TCP_NODELAY
+
+import logging
+import sys
+import time
+
+from roast.roast import pre_round, sign_round
+from roast.transport import send_obj, recv_obj
+from roast.participant import *
+
+MAX_NONCE_QUEUE = 32
+
+
+logging.basicConfig(level=logging.INFO)
+
+if len(sys.argv) != 2:
+    print(f'usage: {sys.argv[0]} <port>')
+    sys.exit(1)
+
+nonce_queue = Queue(MAX_NONCE_QUEUE)
+Process(target=compute_nonce_loop, args=[nonce_queue], daemon=True).start()
+
+port = int(sys.argv[1])
+addr = ('0.0.0.0', port)
+
+sock = socket(AF_INET, SOCK_STREAM)
+sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, True)
+sock.setsockopt(IPPROTO_TCP, TCP_NODELAY, True)
+sock.bind(addr)
+sock.listen()
+
+while True:
+    logging.debug(f'Listening for incoming connections on {addr}')
+
+    connection, src = sock.accept()
+    logging.debug('Accepted connection from {src}')
+
+    try:
+        handle_requests(connection, nonce_queue)
+    except ConnectionResetError as e:
+        print(e)
