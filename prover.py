089ce849c9ea88400282a8269dbfac16a0e7f9d6
Elina <elina.vankempen@gmail.com>
2024-05-05 23:07:13 -0700

    client send hash model to verifier

--- prover.py
+++ prover.py
@@ -1,4 +1,21 @@
-import pyoprf
-
+#import pyoprf
+from hashlib import sha1
+import numpy as np
+import socket
 
 # This is the client proving its participation to the service provider.
+
+model = np.genfromtxt("result.txt")
+hash_model=sha1(model)
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+host = socket.gethostname()
+port = 12345
+
+client_socket.connect((host,port))
+
+hs = hash_model.hexdigest()
+client_socket.send(hs.encode())
+
+client_socket.close()
--- verifier.py
+++ verifier.py
@@ -1,3 +1,27 @@
 import pyoprf
+from hashlib import sha1
+import numpy as np
+import socket
 
 #This is the service provider verifying a client's (prover) participation
+
+
+model = np.genfromtxt("result.txt")
+hash_model = sha1(model)
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+host=socket.gethostname()
+port=12345
+
+server_socket.bind((host,port))
+
+server_socket.listen(5)
+
+while True:
+	client_socket, addr = server_socket.accept()
+	print('connected to prover')
+	data = client_socket.recv(1024).decode()
+	if data == hash_model.hexdigest():
+		print("match")
+	client_socket.close()
